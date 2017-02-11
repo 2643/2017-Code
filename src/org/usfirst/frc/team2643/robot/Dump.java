42320c3a83da3a087035a7b2ee1b59ebfc041fe0
Team 2643 <Team 2643@LAPTOP-LAKCQJ15.esuhsd.org>
2017-02-11 10:59:53 -0800

    Changed dump motor speed and deleted gear method

--- src/org/usfirst/frc/team2643/robot/Dump.java
+++ src/org/usfirst/frc/team2643/robot/Dump.java
@@ -6,22 +6,24 @@ public class Dump extends Robot {
 	}
 
 	public void dump() {
-
 		/*
 		 * If the up dpad is pressed and the top limit switch is false, then the
 		 * motor dump motor moves at a speed of 0.5 until the bottom limit
 		 * switch is gotten.
 		 */
 		if (Robot.opStick.getPOV() == 0 && hallEffectTop.get() == false) {
-			Robot.dumpMotor.set(0.5);
+			Robot.dumpMotor.set(0.25);
 		}
+		
 		/*
 		 * Else, if the down dpad is pressed and the limit switch has not been
 		 * pressed, then
 		 */
 		else if (Robot.opStick.getPOV() == 180 && hallEffectBottom.get() == false) {
-			Robot.dumpMotor.set(-0.5);
-		} else {
+			Robot.dumpMotor.set(-0.25);
+		} 
+		
+		else {
 			Robot.dumpMotor.set(0);
 		}
 	}
