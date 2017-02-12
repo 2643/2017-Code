2f02a811143349351388e9f99ae68ba1e6c0a173
Team 2643 <Team 2643@LAPTOP-LAKCQJ15.esuhsd.org>
2017-02-11 16:39:01 -0800

    Added even more javadoc comments

--- src/org/usfirst/frc/team2643/robot/Intake.java
+++ src/org/usfirst/frc/team2643/robot/Intake.java
@@ -5,38 +5,39 @@ public class Intake extends Robot{
 	} 
 	/**
 	 * This is the intake function
+	 * {@code intake()}: if the intake in button is pressed, then the intakeMotor will be set to positive.
+	 * Else if the intake out button is pressed, then the intakeMotor will be set to negative.
+	 * Else the intake motor will set to 0.
+	 * 
+	 * @see 
+	 * 	  {@code RobotMap}: Intake variables
 	 */
 	public static void intake(){
 		int intake = 3;
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		if(opStick.getRawButton(5)){
+		if(opStick.getRawButton(RobotMap.INTAKE_IN_BUTTON)){
 			intake = 1;
 		}
-		else if(opStick.getRawButton(6)){
+		else if(opStick.getRawButton(RobotMap.INTAKE_OUT_BUTTON)){
 			intake = 2;
 		}
-		else if(opStick.getRawButton(7)){
-			intake = 3;
-		}
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		//if the opStick button 1 is pressed is true, then the robot intake motor will 
 		//be set to the intake motor speed.
 		if (intake == 1)
 		{
 			Robot.intakeMotor.set(RobotMap.INTAKE_IN_SPEED);
-			System.out.println("Intake set to 'IN'");
 		} 
 		//else if the opStick button 2 will make the intake motor preform outake.
 		else if (intake == 2) 
 		{
 			Robot.intakeMotor.set(RobotMap.INTAKE_OUT_SPEED);
-			System.out.println("Intake set to 'OUT'");
 		} 
 		//else the intake motor will be set to no speed.
 		else if(intake == 3) 
 		{
 			Robot.intakeMotor.set(RobotMap.INTAKE_NO_SPEED);
-			System.out.println("Intake set to 'OFF'");
 		}
 	}
 }
