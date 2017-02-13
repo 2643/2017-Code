14cf0dc2bee7bd60d339a7fb18c77b979e4bf179
Sanjana679 <sanjana@nandi.net>
2017-02-12 16:36:38 -0800

    Added specifications for half and full load for dump

--- src/org/usfirst/frc/team2643/robot/Dump.java
+++ src/org/usfirst/frc/team2643/robot/Dump.java
@@ -13,22 +13,17 @@ public class Dump extends Robot {
 	 *		and the bottom limit switch is not pressed, then the dumpMotor will move backwards.
 	 *		Otherwise it will just do nothing
 	 *
+	 *@speed
+	 *		speed for full load - (0.5)
+	 *		speed for about 10 balls - (0.3)
+	 *
 	 */
 	public void dump() {
-		/*
-		 * If the up dpad is pressed and the top limit switch is false, then the
-		 * motor dump motor moves at a speed of 0.5 until the bottom limit
-		 * switch is gotten.
-		 */
 		if (Robot.opStick.getPOV() == 0 && hallEffectTop.get() == false) 
 		{
 			Robot.dumpMotor.set(RobotMap.DUMP_UP_FULL_SPEED);
 		}
 		
-		/*
-		 * Else, if the down dpad is pressed and the limit switch has not been
-		 * pressed, then
-		 */
 		else if (Robot.opStick.getPOV() == 90 && hallEffectTop.get() == false)
 		{
 			Robot.dumpMotor.set(RobotMap.DUMP_UP_HALF_SPEED);
