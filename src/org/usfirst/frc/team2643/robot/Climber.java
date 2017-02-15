b6955fcc3c5d4a2236ded19acd16d3f5d0dc1bc0
Team2643 <team2643@gmail.com>
2017-02-14 18:35:16 -0800

    Hi

--- src/org/usfirst/frc/team2643/robot/Climber.java
+++ src/org/usfirst/frc/team2643/robot/Climber.java
@@ -9,7 +9,7 @@ public class Climber {
 	 * 		If the dPad is pressed up, then the climber motor will set to positive.
 	 * 		Else if the dPad is pressed down, then the climber will set to negative.
 	 * 		Else the climber will be set to 0.
-	 */
+	 */ 
 	public static void climber() {
 		if (Robot.opStick.getPOV() == 0)
 			Robot.climberMotor.set(0.5);
