b6ecc8e38cdc01472d75eebe455328b0faf57139
Govind Pimpale <gpimpale29@gmail.com>
2019-03-16 11:53:37 -0700

    added auto code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,24 +1,30 @@
 package frc.robot;
 
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
 
 public class Robot extends TimedRobot {
 
-// Drive Motors
-Spark l1 = new Spark(5);
-Spark l2 = new Spark(2);
-Spark r1 = new Spark(6);
-Spark r2 = new Spark(1);
+  // Drive Motors
+  Spark l1 = new Spark(5);
+  Spark l2 = new Spark(2);
+  Spark r1 = new Spark(6);
+  Spark r2 = new Spark(1);
 
-Joystick jyst = new Joystick(0);
+  int state = 0;
+
+  Joystick j1 = new Joystick(0);
+
+  NetworkTable visionTable = NetworkTableInstance.getDefault().getTable("vision");
 
   @Override
   public void robotInit() {
     new Thread(() -> {
-			CameraServer.getInstance().startAutomaticCapture();
+      CameraServer.getInstance().startAutomaticCapture();
     }).start();
   }
 
@@ -34,12 +40,38 @@ Joystick jyst = new Joystick(0);
   public void autonomousPeriodic() {
   }
 
+  public void setDrive(double l, double r) {
+    l1.set(l);
+    l2.set(l);
+    r1.set(r);
+    r2.set(r);
+  }
+
   @Override
   public void teleopPeriodic() {
-    l1.set(jyst.getRawAxis(1));
-    l2.set(jyst.getRawAxis(1));
-    r1.set(jyst.getRawAxis(5));
-    r2.set(jyst.getRawAxis(5));
+    switch (state) {
+    case 0: {
+      setDrive(j1.getRawAxis(1), j1.getRawAxis(5));
+      break;
+    }
+    case 1: {
+      double centerLocation = (visionTable.getEntry("centroid-left-x").getDouble(0)
+          + visionTable.getEntry("centroid-right-x").getDouble(0)) / 2;
+      boolean valid = visionTable.getEntry("valid").getBoolean(false);
+      System.out.println("valid: " + valid + "center: " + centerLocation);
+      if (valid) {
+        if (centerLocation > 0.1) {
+          setDrive(0.3, -0.3);
+        } else if (centerLocation < -0.1) {
+          setDrive(-0.3, 0.3);
+        } else {
+          setDrive(0.3, 0.3);
+        }
+      } else {
+        setDrive(0, 0);
+      }
+    }
+    }
   }
 
   @Override
